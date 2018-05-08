175f4f6837c695200fe60c3c8ff153b7a715d4ff
marcus <marcus_loureiro@ecad.org.br>
2018-05-08 15:19:15 -0300

    Adaptando para Java REST Client [6.2] » Java High Level REST Client

--- src/main/java/br/com/api/elastic/bean/ElasticBean.java
+++ src/main/java/br/com/api/elastic/bean/ElasticBean.java
@@ -8,7 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.jboss.logging.Logger;
 
 import br.com.api.elastic.dto.ConsultaDadosDTO;
 import br.com.api.elastic.dto.DebitoUsuarioDTO;
@@ -23,19 +22,13 @@ public class ElasticBean {
 	@Inject
 	private ElasticSearchCrud elasticService;
 	
-
-	private static Logger log = Logger.getLogger(ElasticBean.class);
-
 	@POST
 	@Path("/salvar")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response salvar(String json) {
 		 
-		System.out.println("SERVICO RODANDO..");
-		
 		elasticService.CreateDocument();
-		
 		
 		return Response.ok("OK", MediaType.APPLICATION_JSON).build();
 	}
@@ -46,10 +39,7 @@ public class ElasticBean {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response consultar(ConsultaDadosDTO<InformacaoLoginDTO> request) {
 		 
-		System.out.println("SERVICO RODANDO..");
-		
 		String retorno = elasticService.buscaPorUsuario(request.getMapaParametros().get("LOGIN").toString());
-		
 		
 		return Response.ok(retorno, MediaType.APPLICATION_JSON).build();
 	}
@@ -60,13 +50,9 @@ public class ElasticBean {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response obterDebitoUsuario(ConsultaDadosDTO<DebitoUsuarioDTO> request) {
 		 
-		System.out.println("SERVICO RODANDO..");
-		
 		String retorno = elasticService.obterDebitoUsuario(request.getMapaParametros().get("COD_USUARIO").toString());
-		
 		
 		return Response.ok(retorno, MediaType.APPLICATION_JSON).build();
 	}
-
 
 }
---
+++ src/main/java/br/com/api/elastic/config/ElasticSearchClientConfig.java
@@ -0,0 +1,65 @@
+package br.com.api.elastic.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.ejb.Stateless;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.jboss.logging.Logger;
+
+@Stateless
+public class ElasticSearchClientConfig {
+
+	public String ELASTIC_PROPERTIES = "/elasticSearch.properties";
+
+	private static final Logger log = Logger.getLogger(ElasticSearchClientConfig.class);
+	private RestHighLevelClient restClient = null;
+
+	private Properties elasticProperties = null;
+
+	public ElasticSearchClientConfig() {
+		try {
+			elasticProperties = new Properties();
+			elasticProperties.load(ElasticSearchClientConfig.class.getResourceAsStream(ELASTIC_PROPERTIES));
+			log.info(elasticProperties.getProperty("host"));
+		} catch (IOException ex) {
+			log.info("Exception occurred while load elastic properties : " + ex, ex);
+		}
+	}
+
+	public RestHighLevelClient getInstance() {
+
+		if (restClient == null) {
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			UsernamePasswordCredentials auth = new UsernamePasswordCredentials(elasticProperties.getProperty("user"), elasticProperties.getProperty("password"));
+			credentialsProvider.setCredentials(AuthScope.ANY, auth);
+			
+			HttpHost hosts = new HttpHost(elasticProperties.getProperty("host"), Integer.valueOf(elasticProperties.getProperty("port")));
+			RestHighLevelClient builder = new RestHighLevelClient(RestClient.builder(hosts)
+					.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+						@Override
+						public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+							return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+						}
+					}));
+			restClient = builder;
+		}
+		return restClient;
+	}
+
+	public void closeClient() throws IOException {
+		if (restClient != null) {
+			restClient.close();
+		}
+	}
+
+}
